5df0ef27b3c939fc9bf12739160a66b9cccfa898
tony <66592261+anthony1334@users.noreply.github.com>
2020-11-19 19:02:42 +0100

    add recap

--- vFleet/src/navigation/index.js
+++ vFleet/src/navigation/index.js
@@ -3,15 +3,32 @@ import { createStackNavigator } from 'react-navigation-stack'
 import CheckList from '../screens/CheckList/CheckList'
 
 import Splash from './../screens/Splash/Splash'
+import LoginScreen from './../screens/LoginScreen/LoginScreen'
+import CreateAccountScreen from './../screens/CreateAccountScreen/CreateAccountScreen'
+import Recap from '../screens/recap.js/recap'
 
 const StackNavigator = createSwitchNavigator(
     {
         Splash: {
-            screen: Splash
+            screen: Splash,
+            Login: LoginScreen,
+            CreateAccount: CreateAccountScreen,
         },
+        
+        LoginScreen: {
+            screen: LoginScreen
+        },
+
+       
+
         CheckList: {
             screen: CheckList
+        },
+
+        Recap:{
+            screen: Recap
         }
+        
     },
     {
         initialRouteName: 'Splash',
---
+++ vFleet/src/screens/recap.js/Recap.js
@@ -0,0 +1,67 @@
+import React from 'react'
+import { View, Button } from 'react-native'
+import { Text, FAB } from 'react-native-paper'
+import styles from '../../theme/theme'
+import Header from '../../components/header/Header'
+import CheckList from'../CheckList/CheckList'
+
+
+const Recap = ({ navigation }) => {
+    const items = navigation.getParam('recap')
+    console.log(JSON.stringify(items))
+    const handleBack = () => {
+        console.log("je suis la",[indice])
+        const itemCourant = item
+        itemCourant.value = value
+        items[indice] = itemCourant
+        setItems(items)
+      
+        console.log(JSON.stringify(items))
+        const newIndice = (indice - 1)
+        if (newIndice < items.length) {
+          setIndice(newIndice)
+          setItem(items[newIndice])
+          setValue(item.value)
+          setPrevious(value)
+          
+        }
+          if(indice<1){
+            
+                  navigation.navigate('Splash')
+      
+         } 
+          
+        
+      }
+  
+
+  return (
+    <>
+
+
+      <Header titleText="vFleetCheck" navigation={navigation} />
+      <View style={styles.container}>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+   
+         
+      
+        </View> 
+        <View> {JSON.stringify(items)}</View>
+        
+
+        <FAB
+          style={styles.fab}
+          small
+          icon='check'
+          label='Valider'
+          onPress={() => navigation.navigate('Splash')}
+        />
+      </View>
+
+    </>
+  )
+}
+
+
+
+export default Recap
