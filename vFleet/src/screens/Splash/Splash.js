f64bee66cf8a24aa480025de29b0d7dd7fe345e6
tony <66592261+anthony1334@users.noreply.github.com>
2020-11-19 19:19:13 +0100

    add gitPull

--- vFleet/src/screens/Splash/Splash.js
+++ vFleet/src/screens/Splash/Splash.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useState} from 'react'
 import { View, Button } from 'react-native'
 import { Text, FAB } from 'react-native-paper'
 import styles from './../../theme/theme'
